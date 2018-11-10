a1ecf63ed7696b6836741698fc16c53dee396b70
Hayden Bruin <bruinhayden@gmail.com>
2018-11-10 17:33:27 +1300

    Storing on github

---
+++ assets/inc/head.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mobile Automotive Air Conditioning Services - Tauranga and Mount Maunganui</title>
+    <meta name="keywords" content="car air conditioning, Tauranga, Mount Maunganui, vehicle air conditioning, Alpine Air, auto air conditioning repairs, auto air conditioning service, air conditioning servicing, air conditioning, air conditioner, cooling, automotive air conditioning, auto air conditioning, mobile business, Hamilton, Huntly, Matamata, Morrinsville, Cambridge, Te Awamutu, Te Kauwhata, Ngaruawahia, re-gassing, repairs, preventative maintenance, servicing, odour elimination, cabin filters, pollen filters, auto parts, automotive replacements, automotive">
+    <meta name="description" content="For mobile automotive air conditioning servicing and repairs in Tauranga, Alpine Air Tauranga has been the trusted name since 2000. Call the team on 022 309 7494.">
+
+    <!-- Bootstrap -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/css/style.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-107937190-1', 'auto');
+    ga('send', 'pageview');
+    </script>
+</head>
+<body>
---
+++ assets/inc/header.php
@@ -0,0 +1,29 @@
+<header>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-7">
+                <nav>
+                    <a href="#"><li>Home</li></a>
+                    <a href="#"><li>What we offer</li></a>
+                    <a href="#"><li>About</li></a>
+                    <a href="#"><li>Locations</li></a>
+                    <a href="#"><li>Contact</li></a>
+                </nav>
+            </div>
+            <div class="col-xs-5 details">
+                <a href="#">
+                    <div class="contact email">
+                        <div class="title">Email us for a free quote</div>
+                        <div class="address">help@alpineairnz.co.nz</div>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="contact phone">
+                        <div class="title">Call us for a free quote</div>
+                        <div class="address">022 309 7494</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</header>
